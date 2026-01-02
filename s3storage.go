a55702058b5d036d08b5c5d27bd48cdbe4eeef51
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2026-01-01 21:10:08 -0800

    fix adds a HeadObject call for each blob to fetch the content type from S3 metadata. This will now return the correct MIME type (e.g., image/jpeg, video/mp4) instead of leaving it empty.will make the /list/ endpoint slightly slower since it makes an additional API call per blob

--- s3storage.go
+++ s3storage.go
@@ -156,9 +156,20 @@ func (s *S3Storage) ListBlobs(ctx context.Context) ([]BlobInfo, error) {
 					blobURL = s.publicURL + "/" + key
 				}
 
+				// Get content type from S3 object metadata
+				contentType := "application/octet-stream"
+				headResult, err := s.client.HeadObject(ctx, &s3.HeadObjectInput{
+					Bucket: aws.String(s.bucket),
+					Key:    aws.String(key),
+				})
+				if err == nil && headResult.ContentType != nil {
+					contentType = *headResult.ContentType
+				}
+
 				blobs = append(blobs, BlobInfo{
 					SHA256:   key,
 					Size:     aws.ToInt64(obj.Size),
+					Type:     contentType,
 					URL:      blobURL,
 					Uploaded: obj.LastModified.Unix(),
 				})
